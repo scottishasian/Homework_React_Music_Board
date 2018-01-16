4445480a83066fc8ffd892b67f8e0d8be7b8e6f6
Kynan <k.song@blueyonder.co.uk>
2018-01-16 19:43:21 +0000

    Set up basic files.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import MusicBox from './MusicComponents/MusicBox.jsx'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <MusicBox />
+
+    );
+  }
+}
+
+export default App;
---
+++ src/MusicComponents/MusicBox.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class MusicBox extends React.Component {
+
+  render() {
+    return(
+      <div className="music-box">
+        Hello
+      </div>
+    )
+
+  };
+
+}
+
+export default MusicBox;
